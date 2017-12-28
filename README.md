8351695211cdefca2045d9c5c2770d1674b0b367
Alex Myodov <amyodov@gmail.com>
2017-12-28 16:36:54 +0200

    OpenZeppelin-based contract.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-## universa/network/utn-p/utnp
+# UTN-P (“Universa TokeN, Placeholder”)
+
+Based on [OpenZeppelin](https://github.com/OpenZeppelin/zeppelin-solidity) framework.
+
+## Build
+~~~
+solidity_flattener utn-p.sol --output build/utn-p-combined.sol 
+solc --optimize --output-dir build --overwrite --gas --bin --abi build/utn-p-combined.sol
+~~~
+
+Token Timelock
+~~~
+python3 -c "from datetime import datetime; print(int(datetime(2018, 4, 1).timestamp()))"
+~~~
+
+Result: `1522530000`
---
+++ utn-p.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.4.18;
+
+import './zeppelin-solidity/contracts/token/BasicToken.sol';
+import './zeppelin-solidity/contracts/token/BurnableToken.sol';
+import './zeppelin-solidity/contracts/token/ERC20.sol';
+
+/**
+ * @dev UTN-P ERC20 token.
+ *
+ * Based on OpenZeppelin framework.
+ *
+ * Features:
+ *
+ * * ERC20 compatibility, with token details as properties.
+ * * total supply: 4997891952 (initially given to the contract author).
+ * * decimals: 18
+ * * BurnableToken: some addresses are allowed to burn tokens.
+ * * transferFrom/approve/allowance methods are present but do nothing.
+ * * TimeLock: implemented externally (in TokenTimelock contract), some tokens are time-locked for 3 months.
+ */
+//TokenVesting
+contract UTNP is BasicToken, BurnableToken, ERC20 {
+
+    string public name = "UTN-P: Universa Token";
+    string public symbol = "UTNP";
+    uint8 public decimals = 18;
+
+    uint256 INITIAL_SUPPLY_UTN = 4997891952;
+
+    mapping(address => bool) public burnerAddresses;
+    address public timeLockedAddress = 0xDf5963B72B2478E828Bc69693f1f47C2b2BB7948;
+
+    /**
+     * @dev Constructor that:
+     * * gives all of existing tokens to the message sender;
+     * * initializes the burners (also adding the message sender);
+     */
+    function UTNP() public {
+        totalSupply = INITIAL_SUPPLY_UTN * (10 ** uint256(decimals));
+        balances[msg.sender] = totalSupply;
+
+        // Initialize the burners.
+        burnerAddresses[msg.sender] = true;
+        burnerAddresses[0xB5a7235d2D53aaDaEfe68c4477BdAF1A8046DEFC] = true;
+        burnerAddresses[0x5335266ef86655ac6764b98cD9FF1166EF542342] = true;
+        burnerAddresses[0xE1D2200354677D549782bA3eF0b782b203C1057a] = true;
+    }
+
+    /**
+     * @dev Standard method to comply with ERC20 interface;
+     * prevents any Ethereum-contract-initiated operations.
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        return false;
+    }
+
+    /**
+     * @dev Standard method to comply with ERC20 interface;
+     * prevents any Ethereum-contract-initiated operations.
+     */
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        return false;
+    }
+
+    /**
+     * @dev Standard method to comply with ERC20 interface;
+     * prevents any Ethereum-contract-initiated operations.
+     */
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return 0;
+    }
+
+    /**
+     * @dev Burns a specific amount of tokens.
+     * Only an address listed in `burnerAddresses` can do this.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) public {
+        require(burnerAddresses[msg.sender]);
+        super.burn(_value);
+    }
+}
